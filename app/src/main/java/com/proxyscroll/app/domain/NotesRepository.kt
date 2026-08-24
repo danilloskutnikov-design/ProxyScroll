cdbdf8fc9a0e26b4bdacf2811c7e1b70673cf2a7
danilloskutnikov-design <danil.loskutnikov.gpt@gmail.com>
2026-08-25 00:13:21 +0400

    Extend repository with group storage

--- app/src/main/java/com/proxyscroll/app/domain/NotesRepository.kt
+++ app/src/main/java/com/proxyscroll/app/domain/NotesRepository.kt
@@ -4,6 +4,9 @@ interface NotesRepository {
     /** Active notes only. */
     fun getAll(): List<Note>
     fun getTrash(): List<Note>
+    fun getGroups(): List<NoteGroup>
+    fun upsertGroup(group: NoteGroup)
+    fun deleteGroup(groupId: String)
     fun upsert(note: Note)
     fun upsertAll(notes: Collection<Note>)
     fun moveToTrash(noteIds: Set<String>, deletedAt: Long)
