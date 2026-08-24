f0fafea9903ce11fdfb159c457077fa71186799e
danilloskutnikov-design <danil.loskutnikov.gpt@gmail.com>
2026-08-24 18:39:12 +0400

    Bump Alpha 19 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.proxyscroll.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 18
-        versionName = "0.7.1-alpha18"
+        versionCode = 19
+        versionName = "0.7.2-alpha19"
     }
 
     val alphaKeystore = file("proxyscroll-alpha.keystore")
